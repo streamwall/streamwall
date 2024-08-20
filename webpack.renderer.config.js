6cc2d8308340e8921cee50b4a21a108e43373539
sayhiben <bmenesini@gmail.com>
2024-08-20 11:42:41 -0700

    Fix renderer build issue

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -4,22 +4,22 @@ const baseConfig = require('./webpack.base.config');
 module.exports = {
   ...baseConfig({
     babel: {
-      presets: [['@babel/preset-env', { targets: { electron: '31.3' }}]],
+      presets: [['@babel/preset-env', { targets: { electron: '31.3' } }]],
       plugins: [
         ['@babel/plugin-transform-react-jsx', { pragma: 'h', pragmaFrag: 'Fragment' }],
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-object-rest-spread'
       ]
     },
-    target: 'electron-renderer',
-    entry: {
-      background: './src/renderer/background.js',
-      overlay: './src/renderer/overlay.js',
-      playHLS: './src/renderer/playHLS.js'
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist/renderer'),
-      filename: '[name].js'
-    }
   }),
+  target: 'electron-renderer',
+  entry: {
+    background: './src/renderer/background.js',
+    overlay: './src/renderer/overlay.js',
+    playHLS: './src/renderer/playHLS.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/renderer'),
+    filename: '[name].js'
+  }
 }
